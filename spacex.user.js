d56b77906058e558ef481664b46251fb53087fe7
fiercedude <102837657+fiercedude@users.noreply.github.com>
2022-04-02 04:28:56 -0400

    Update spacex.user.js

--- spacex.user.js
+++ spacex.user.js
@@ -16,7 +16,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/502460438425108490/959730494579302421/trans_spacex_logo_v4.png";
+            i.src = "https://cdn.discordapp.com/attachments/502460438425108490/959731047464062997/trans_spacex_logo_v4.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
