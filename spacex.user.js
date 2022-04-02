c8da62363a921464be43d70ec7ea47818c2e32d1
fiercedude <102837657+fiercedude@users.noreply.github.com>
2022-04-02 19:09:40 -0400

    Update spacex.user.js
    updated logo

--- spacex.user.js
+++ spacex.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/place SpaceX Template
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  Template for spacex graphic
 // @author       /u/fiercedude
 // @match        https://hot-potato.reddit.com/embed*
@@ -16,7 +16,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://cdn.discordapp.com/attachments/502460438425108490/959769497328316426/trans_spacex_logo_v6.png";
+            i.src = "https://raw.githubusercontent.com/adamandreasson/rplacespace/main/trans_space_logo_dynamic.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
