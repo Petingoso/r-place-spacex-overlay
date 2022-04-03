85118074ea681487c6e012ba01f4d711e79860fb
Petingoso <92183955+Petingoso@users.noreply.github.com>
2022-04-03 19:54:06 +0000

    Update spacex.user.js

--- spacex.user.js
+++ spacex.user.js
@@ -17,7 +17,7 @@ if (window.top !== window.self) {
         (function () {
             const i = document.createElement("img");
             i.src = "https://raw.githubusercontent.com/adamandreasson/rplacespace/main/trans_space_logo_dynamic.png";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
         })())
