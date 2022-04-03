dbc15c7a0e67115129c91ffa28b3ac106551ffb9
Petingoso <92183955+Petingoso@users.noreply.github.com>
2022-04-03 19:54:58 +0000

    Update spacex.user.js

--- spacex.user.js
+++ spacex.user.js
@@ -6,8 +6,8 @@
 // @author       /u/fiercedude
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/fiercedude/r-place-spacex-overlay/raw/main/spacex.user.js
-// @downloadURL  https://github.com/fiercedude/r-place-spacex-overlay/raw/main/spacex.user.js
+// @updateURL    https://github.com/Petingoso/r-place-spacex-overlay/edit/main/spacex.user.js
+// @downloadURL  https://github.com/Petingoso/r-place-spacex-overlay/edit/main/spacex.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -16,7 +16,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/adamandreasson/rplacespace/main/trans_space_logo_dynamic.png";
+            i.src = "https://cdn.discordapp.com/attachments/749316099828351067/960266010760458322/unknown.png?size=4096";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
